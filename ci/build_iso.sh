51292efa9c2c7f8bb58cfd6ccf91e0d3dbf0fb89
Tancredi-Paul Grozav <paul@grozav.info>
2025-09-15 12:53:42 +0000

    wip

--- ci/build_iso.sh
+++ ci/build_iso.sh
@@ -6,47 +6,43 @@ set -x &&
 script_dir="$( cd $( dirname ${0} ) && pwd )" &&
 project_root="$( cd ${script_dir}/.. && pwd )" &&
 
-# sudo apt-get install -y \
-#   ` # git - clone ipxe project ` \
-#   ` # git ` \
-#   ` # perl - required for building ipxe ` \
-#   perl \
-#   ` # make - Build system for the ipxe project ` \
-#   make \
-#   ` # gcc - Compiler to build the ipxe C code ` \
-#   gcc \
-#   ` # libc-dev - Basic standard C library for ipxe ` \
-#   libc-dev \
-#   ` # xz-dev - (Alpine) required by the ipxe project ` \
-#   ` # xz-dev ` \
-#   ` # liblzma-dev - (ubuntu) required by the ipxe project ` \
-#   liblzma-dev \
-#   ` # syslinux - required to make the iso bootable ` \
-#   syslinux \
-#   ` # xorriso - to create the iso file ` \
-#   xorriso \
-#   &&
+sudo apt-get install -y \
+  ` # git - clone ipxe project ` \
+  ` # git ` \
+  ` # perl - required for building ipxe ` \
+  perl \
+  ` # make - Build system for the ipxe project ` \
+  make \
+  ` # gcc - Compiler to build the ipxe C code ` \
+  gcc \
+  ` # libc-dev - Basic standard C library for ipxe ` \
+  libc-dev \
+  ` # xz-dev - (Alpine) required by the ipxe project ` \
+  ` # xz-dev ` \
+  ` # liblzma-dev - (ubuntu) required by the ipxe project ` \
+  liblzma-dev \
+  ` # syslinux - required to make the iso bootable ` \
+  syslinux \
+  ` # xorriso - to create the iso file ` \
+  xorriso \
+  &&
 
 git clone https://github.com/ipxe/ipxe.git ${project_root}/ipxe &&
 
 # nano config/console.h # uncomment CONSOLE_SERIAL if you need it.
 
-# Make all (default config)
-# make &&
-
-
 # See: https://ipxe.org/appnote/buildtargets
 # Make .iso
 (
   cd ${project_root}/ipxe/src &&
-  # make \
-  #   -j$(nproc) \
-  #   DEBUG=dhcp,tftp,http \
-  #   ` # This needs to be a relative path, as defined in Makefile ` \
-  #   bin/ipxe.iso \
-  #   EMBED=${project_root}/ci/embedded.ipxe \
-  #   &&
-  mkdir -p ./bin && touch ./bin/ipxe.iso &&
+  make \
+    -j$(nproc) \
+    DEBUG=dhcp,tftp,http \
+    ` # This needs to be a relative path, as defined in Makefile ` \
+    bin/ipxe.iso \
+    EMBED=${project_root}/ci/embedded.ipxe \
+    &&
+  # mkdir -p ./bin && touch ./bin/ipxe.iso &&
   true
 )
 
