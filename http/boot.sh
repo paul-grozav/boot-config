ce09f3b81be9960eaf856c9175205928cca14020
Tancredi-Paul Grozav <paul@grozav.info>
2026-02-12 10:08:20 +0200

    g

--- http/boot.sh
+++ http/boot.sh
@@ -4,6 +4,24 @@
 # ============================================================================ #
 # curl -s https://paul-grozav.github.io/boot-config/boot.sh | sh
 # ============================================================================ #
+set -x &&
+iso_file="$(mktemp)" &&
+
+echo "Downloading iso ..." &&
+curl \
+  --silent \
+  --output ${iso_file} \
+  https://paul-grozav.github.io/boot-config/ipxe.iso \
+  &&
+
 echo "Booting ..." &&
+( qemu-system-x86_64 \
+  -m 2G \
+  -cdrom ${iso_file} \
+  || true ) &&
+
+rm -f ${iso_file} &&
+
+set +x &&
 true
 # ============================================================================ #
